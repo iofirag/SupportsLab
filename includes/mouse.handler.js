da98a7f5fa4245e163d48d78318fcbee1fae5654
ofir agahi <iofirag@gmail.com>
2015-01-11 06:51:23 +0200

    real data. create city data. create diagram. normalize data

--- includes/mouse.handler.js
+++ includes/mouse.handler.js
@@ -1,15 +1,30 @@
+/* Data-base */
+townsData = [];
+
 /* Default parameters */ 
+center = [35, 31.7];
 scale = 9400;
-translate = [-120,-270];
+translate = [0,0];
+ZOOM_BY = 1.5;
+zoomTimes = 1;
+
+diagram = {
+		a : 'sum1',
+		b : 'sum2',
+		c : 'sum3',
+		d : 'sum4'
+};
 
 $(document).ready(function() {
+	/* load database */
+	readFromDatabase();
+
 	/* Create the first map with default parameters */
 	createMap();
 	createDiagram();
 	
 	/* Add Event Listeners */
 	parent.addEventListener('mousedown', mouseDown, false);
-	
 	// Safari
 	parent.addEventListener('mousewheel', mouseWheel, false);
 	// Firefox
@@ -52,14 +67,14 @@ $(document).ready(function() {
 
 /* Mouse Down */
 var mouseDown = function(e) {
-	console.log("Down");
+	//console.log("Down");
 	var prevMouse = {
 		x : e.clientX,
 		y : e.clientY
 	};
 	/* Mouse Move */
 	var mouseMove = function(e) {
-		console.log("Move");
+		//console.log("Move");
 		// Calculate distanse between the old coordinate and the new coordinate
 		x = e.clientX-prevMouse.x;
 		y = e.clientY-prevMouse.y;
@@ -70,8 +85,15 @@ var mouseDown = function(e) {
 		e.preventDefault();
 		
 		// Change the translate values
-		translate[0]+= x;
-		translate[1]+= y;
+		// var addX=0; 
+		// if (x>0) addX=-0.01; 
+		// else if (x<0) addX=0.01;
+		// var addY=0; 
+		// if (y>0) addY=0.01;
+		// else if (y<0) addY=-0.01;
+		
+		center[0]+= -x/150;//x;
+		center[1]+= y/150;//(-y);
 		
 		// Change the old map with new one
 		$("#zoom_map").empty();
@@ -79,7 +101,7 @@ var mouseDown = function(e) {
 	};
 	/* Mouse Up */
 	var mouseUp = function(e) {
-		console.log("Up");
+		//console.log("Up");
 		document.body.style.cursor = null;
 		document.removeEventListener('mousemove', mouseMove, false);
 		document.removeEventListener('mouseup', mouseUp, false);
@@ -114,7 +136,8 @@ var mouseWheel = function(e) {
 
 	if (delta > 0) {
 		//zoomBy(0.9, localX, localY);
-		scale*=1.1;
+		scale*=ZOOM_BY;
+		zoomTimes++;
 		/****Way A **********
 		translate[0]-= 70;
 		translate[1]-= 70;
@@ -126,7 +149,11 @@ var mouseWheel = function(e) {
 		
 	} else if (delta < 0) {
 		//zoomBy(1.1, localX, localY);
-		scale/=1.1;
+		scale/=ZOOM_BY;
+		//if (zoomTimes>1) 
+		zoomTimes--;		
+		//else zoomTimes=1;
+		
 		/**Way A **********
 		translate[0]+= 70;
 		translate[1]+= 70;
@@ -135,7 +162,7 @@ var mouseWheel = function(e) {
 		translate[1]/=1.1;
 		/*******************/
 	}
-	console.log(scale);
+	//console.log(scale);
 
 	// cancel page scroll
 	e.preventDefault();
@@ -149,20 +176,10 @@ var mouseWheel = function(e) {
 function createMap(){
 	//console.log("start createMap()");
 	$("#zoom_map").css("height", $(document).height()+'px');
-	BLUE = "#264478";
-	RED = "#FF6E51";
-	YELLOW = "#FDC067";
-	GREEN = "#2DB69C";
-	
-	//ISRAEL = "#f5f0bb";
-	//OTHER = "#e7e5d4";
-	
-	townsData = [
-		{id: 'rishon-le-zion', name: 'ראשון לציון', latitude: 31.9500, longitude: 34.8000, radius: 3, 'fillKey': 'gt50',  "sum1":24,"color2":"blue", "sum2":10,"color1":"green",  "sum3":15,"color4":"purple", "sum4":18,"color3":"red"},
-		{id: 'haifa', name: 'חיפה', latitude: 32.8167, longitude: 34.9833, radius: 3, 'fillKey': 'gt50', "sum1":5,"color2":"blue", "sum2":15,"color1":"green", "sum3":6,"color4":"purple", "sum4":14,"color3":"red"},
-		{id: 'jerusalem', name: 'ירושלים', latitude: 31.7833, longitude: 35.2167, radius: 3, 'fillKey': 'gt50', "sum1":16,"color2":"blue", "sum2":10,"color1":"green", "sum3":9,"color4":"purple", "sum4":14,"color3":"red"},
-		{id: 'beer-sheva', name: 'באר-שבע', latitude: 31.2589, longitude: 34.7997, radius: 3, 'fillKey': 'gt50', "sum1":12,"color2":"blue", "sum2":11,"color1":"green", "sum3":10,"color4":"purple", "sum4":17,"color3":"red"}
-	];
+	ORANGE = "#fcaa1c";
+	GRAY = "#434d53";
+	GREEN = "#7bb37f";
+	BLUE = "#23959c";
 			
 	var zoom = new Datamap({
 		element: document.getElementById("zoom_map"),
@@ -170,10 +187,10 @@ function createMap(){
 		// Zoom-in on Israel
 		setProjection: function(element) {
 			var projection = d3.geo.equirectangular()
-							.center([31, 35])
+							.center(center)
 							.rotate([0, 0])
-							.scale(scale)	//9400 standard
-							.translate(translate);
+							.scale(scale);
+							//.translate(translate);
 							//.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 			var path = d3.geo.path().projection(projection);
 			
@@ -194,19 +211,14 @@ function createMap(){
 		}
 	});
 			
-			
-			
-			
-			
-			
+				
 	/************************************/
 		
+	var svgContainer = d3.select("svg");
+	var allCities = svgContainer.append("g").attr("class", "cities");
+	var cityContainer = allCities.append("g").attr("class", "cityData");
 		
 	for (i=0; i<townsData.length; i++){
-		// var rx= townsData[i].latitude;
-		// var ry= townsData[i].longitude-30;
-
-		var svgContainer = d3.select("svg");
 
 		/*****************************************/
 		//Find all town tags
@@ -214,7 +226,7 @@ function createMap(){
 		var town = townList.children[i];
 		
 		//Way-A - put id attribute for every tag (from json)
-		town.id = townsData[i].id;
+		//town.id = townsData[i].id;
 		
 		// //Way-B - take id data from 'dataset.info' in the tag
 		// var townJson_String = town.dataset.info;
@@ -225,39 +237,142 @@ function createMap(){
 		var cx = town.cx.baseVal.value;
 		var cy = town.cy.baseVal.value-20;
 		/*****************************************/
-
 		
-		var circleAttributes = svgContainer.append("rect")	//1- blue
+		
+		var circleAttributes = cityContainer.append("rect")	//1- ORANGE
 			.attr("x", cx)
 			.attr("y", cy - townsData[i].sum1)
 			.attr("width", townsData[i].sum1)
 			.attr("height", townsData[i].sum1)
-			.style("fill", BLUE);
+			.style("fill", ORANGE);
 		
-		var circleAttributes = svgContainer.append("rect")	//2- RED
+		var circleAttributes = cityContainer.append("rect")	//2- GRAY
 			.attr("x", cx)
 			.attr("y", cy)
 			.attr("width", townsData[i].sum2)
 			.attr("height", townsData[i].sum2)
-			.style("fill", RED);
+			.style("fill", GRAY);
 
-		var circleAttributes = svgContainer.append("rect")	//3- YELLOW
+		var circleAttributes = cityContainer.append("rect")	//3- GREEN
 			.attr("x", cx - townsData[i].sum3)
 			.attr("y", cy)
 			.attr("width", townsData[i].sum3)
 			.attr("height", townsData[i].sum3)
-			.style("fill", YELLOW);	//townsData[i].color3
+			.style("fill", GREEN);	//townsData[i].color3
 
-		var circleAttributes = svgContainer.append("rect")	//4- GREEN
+		var circleAttributes = cityContainer.append("rect")	//4- BLUE
 			.attr("x", cx - townsData[i].sum4)
 			.attr("y", cy - townsData[i].sum4)
 			.attr("width", townsData[i].sum4)
 			.attr("height", townsData[i].sum4)
-			.style("fill", GREEN);
+			.style("fill", BLUE);
 	}
 	//console.log("finish createMap()");
 }
 
 function createDiagram(){
-	var svgContainer = d3.select("container").append("svg").attr("width", 700).attr("height", 700);
+	var centerPos = 100;
+	var svgContainer = d3.select('#diagram').append("svg").append("g");//.attr("width", 700).attr("height", 700);
+	var diagramCounter = {
+		a : 0,
+		b : 0,
+		c : 0,
+		d : 0
+	};
+	
+	for (i=0; i<townsData.length; i++){
+		$.each(townsData[i], function(key, val) {
+			if (key == diagram.a) diagramCounter.a+= val; /* if (diagramCounter.a>=500) diagramCounter.a*=0.1;} */
+			else if (key == diagram.b) diagramCounter.b+= val; /* if (diagramCounter.b>=500) diagramCounter.b*=0.1;}*/
+			else if (key == diagram.c) diagramCounter.c+= val; /* if (diagramCounter.c>=500) diagramCounter.c*=0.1;}*/
+			else if (key == diagram.d) diagramCounter.d+= val; /* if (diagramCounter.d>=500) diagramCounter.d*=0.1;}*/
+		    //console.log(key);
+		});
+	}
+	
+	/* normalization */
+	//diagramCounter.a /= 1000000;
+	//diagramCounter.b /= 1000000;
+	//diagramCounter.c /= 1000000;
+	//diagramCounter.d /= 1000000;
+	
+	var circleAttributes = svgContainer.append("rect")	//1- ORANGE
+			.attr("x", centerPos)
+			.attr("y", centerPos-diagramCounter.a)
+			.attr("width", diagramCounter.a)
+			.attr("height", diagramCounter.a)
+			.style("fill", ORANGE);
+		
+		var circleAttributes = svgContainer.append("rect")	//2- GRAY
+			.attr("x", centerPos)
+			.attr("y", centerPos)
+			.attr("width", diagramCounter.b)
+			.attr("height", diagramCounter.b)
+			.style("fill", GRAY);
+
+		var circleAttributes = svgContainer.append("rect")	//3- GREEN
+			.attr("x", centerPos-diagramCounter.c)
+			.attr("y", centerPos)
+			.attr("width", diagramCounter.c)
+			.attr("height", diagramCounter.c)
+			.style("fill", GREEN);	//townsData[i].color3
+
+		var circleAttributes = svgContainer.append("rect")	//4- BLUE
+			.attr("x", centerPos-diagramCounter.d)
+			.attr("y", centerPos-diagramCounter.d)
+			.attr("width", diagramCounter.d)
+			.attr("height", diagramCounter.d)
+			.style("fill", BLUE);
+}
+
+function readFromDatabase() {
+	$.ajax({
+        type: "GET",
+        // url: 'includes/townsData.json',
+        url: 'includes/townsData.json',
+        async: false,
+        success : function(data) {
+			for (i in data) {
+				// debugger;
+				townsData.push(data[i]);
+			}
+			
+			/* normalization */
+			for (i=0; i<townsData.length; i++){
+				//1 Find the hight val
+				var max = Math.max(townsData[i].sum1,townsData[i].sum2,townsData[i].sum3,townsData[i].sum4);
+				var counter=0;
+				
+				//2 Divide it with 10 untill it less then 20 (save how many times divided)
+				while (max>20){ 
+					max/=10;
+					counter++;
+				}
+				
+				//3 Divide all other with same number
+				
+				for (j=0; j<counter; j++){
+					townsData[i].sum1/= (10);
+					townsData[i].sum2/= (10);
+					townsData[i].sum3/= (10);
+					townsData[i].sum4/= (10);
+				}
+				
+				// townsData[i].sum1/= (10*counter);
+				// townsData[i].sum2/= (10*counter);
+				// townsData[i].sum3/= (10*counter);
+				// townsData[i].sum4/= (10*counter);
+				
+				//while (townsData[i].sum1>20) townsData[i].sum1/=10;
+				//while (townsData[i].sum2>20) townsData[i].sum2/=10;
+				//while (townsData[i].sum3>20) townsData[i].sum3/=10;
+				//while (townsData[i].sum4>20) townsData[i].sum4/=10;
+				
+				//townsData[i].sum1 /= 10000000;
+				//townsData[i].sum2 /= 10000000;
+				//townsData[i].sum3 /= 10000000;
+				//townsData[i].sum4 /= 10000000;
+			}
+		}
+    });
 }
